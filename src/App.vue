0a0fd379be048ac7a856c5ce435943e75915d651
Internship314 <6304101315f@gmail.com>
2023-09-21 12:32:20 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mx-auto">
+    <ChatBox />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ChatBox from './components/ChatBox.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    ChatBox,
+  },
+};
 </script>
+
 
 <style>
 #app {
